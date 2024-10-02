f5b35737a7a1d371912d0d4b9d5185040560bf77
8u3631984 <8u3631984@gmail.com>
2024-10-02 11:06:32 +0200

    remove unused imports

--- src/main/java/my/photo_manager/repository/PhotoRepository.java
+++ src/main/java/my/photo_manager/repository/PhotoRepository.java
@@ -3,7 +3,5 @@ package my.photo_manager.repository;
 import my.photo_manager.photo.Photo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface PhotoRepository extends JpaRepository<Photo, Long> {
 }
