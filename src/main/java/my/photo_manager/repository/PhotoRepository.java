414bf6391218650876859b812898c89c77d1fe2e
8u3631984 <8u3631984@gmail.com>
2024-10-02 11:05:00 +0200

    add PhotoRepository

---
+++ src/main/java/my/photo_manager/repository/PhotoRepository.java
@@ -0,0 +1,9 @@
+package my.photo_manager.repository;
+
+import my.photo_manager.photo.Photo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PhotoRepository extends JpaRepository<Photo, Long> {
+}
