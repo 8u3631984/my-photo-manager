de0e28bccca47e97c7fcd2f35e187759e4300b04
8u3631984 <8u3631984@gmail.com>
2024-10-07 13:18:06 +0200

    update configuratioen

--- src/main/java/my/photo_manager/config/PhotoConfiguration.java
+++ src/main/java/my/photo_manager/config/PhotoConfiguration.java
@@ -1,7 +1,15 @@
 package my.photo_manager.config;
 
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
 @Configuration
+@ConfigurationProperties(prefix = "photo")
 public class PhotoConfiguration {
+
+    @Getter
+    private List<String> source;
 }
