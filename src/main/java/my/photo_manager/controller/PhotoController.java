9ecb2e3e32b9c31efc680fed38976385ef2d62be
8u3631984 <8u3631984@gmail.com>
2024-10-07 11:08:05 +0200

    phtot dto add raw data

--- src/main/java/my/photo_manager/controller/PhotoController.java
+++ src/main/java/my/photo_manager/controller/PhotoController.java
@@ -1,6 +1,8 @@
 package my.photo_manager.controller;
 
 import lombok.NonNull;
+import lombok.SneakyThrows;
+import my.photo_manager.photo.Photo;
 import my.photo_manager.photo.PhotoDTO;
 import my.photo_manager.service.PhotoService;
 import org.springframework.ui.Model;
@@ -8,8 +10,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
+import java.util.Base64;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static org.apache.commons.io.FileUtils.readFileToByteArray;
 
 @RestController
 @RequestMapping("/photo")
@@ -33,9 +39,14 @@ public class PhotoController {
     public String index(Model model) {
         model.addAttribute("photos", photoService.getAll()
                 .stream()
-                .map(photo -> new PhotoDTO(photo.getID(), "RAW DATA"))
+                .map(this::mapPhotoToDTO)
                 .collect(Collectors.toList()));
 
         return "index";
+    }
+
+    @SneakyThrows
+    private PhotoDTO mapPhotoToDTO(@NonNull Photo photo) {
+        return new PhotoDTO(photo.getID(), Base64.getEncoder().encodeToString(readFileToByteArray(new File(photo.getFilePath()))));
     }
 }
