2e13ae1fb59d7a406a7c3e0ca07e49d10db5f295
8u3631984 <8u3631984@gmail.com>
2024-10-07 13:58:44 +0200

    change PhotoController to controller, remove rest tests

--- src/main/java/my/photo_manager/controller/PhotoController.java
+++ src/main/java/my/photo_manager/controller/PhotoController.java
@@ -3,36 +3,25 @@ package my.photo_manager.controller;
 import lombok.NonNull;
 import lombok.SneakyThrows;
 import my.photo_manager.model.photo.Photo;
-import my.photo_manager.web.PhotoDTO;
 import my.photo_manager.services.PhotoService;
+import my.photo_manager.web.PhotoDTO;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.io.File;
 import java.util.Base64;
-import java.util.List;
 import java.util.stream.Collectors;
 
 import static org.apache.commons.io.FileUtils.readFileToByteArray;
 
-@RestController
-@RequestMapping("/photo")
+@Controller
 public class PhotoController {
 
     private final PhotoService photoService;
 
     protected PhotoController(@NonNull PhotoService photoService) {
         this.photoService = photoService;
-    }
-
-    @GetMapping("/getAll")
-    public List<PhotoDTO> getAllPhotos() {
-        return photoService.getAll()
-                .stream()
-                .map(photo -> new PhotoDTO(photo.getID(), "RAW DATA"))
-                .collect(Collectors.toList());
     }
 
     @RequestMapping(value = "/")
