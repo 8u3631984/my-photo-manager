652172c8c3059ffd8b76c254f5f9a6c7cb668463
8u3631984 <74074904+8u3631984@users.noreply.github.com>
2024-10-07 13:59:35 +0200

    20 build and save phot if not exists (#21)
    * check if a photo object exists already
    
    * change PhotoController to controller, remove rest tests
    
    ---------
    
    Co-authored-by: 8u3631984 <8u3631984@gmail.com>

--- src/main/java/my/photo_manager/model/repository/PhotoRepository.java
+++ src/main/java/my/photo_manager/model/repository/PhotoRepository.java
@@ -3,5 +3,9 @@ package my.photo_manager.model.repository;
 import my.photo_manager.model.photo.Photo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PhotoRepository extends JpaRepository<Photo, Long> {
+
+    Optional<Photo> findByHashValue(String hashValue);
 }
