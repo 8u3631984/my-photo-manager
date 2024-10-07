74c13a3c9e1403a8b75f9f546cbb17870fef8331
8u3631984 <8u3631984@gmail.com>
2024-10-07 13:37:50 +0200

    enable scheduling

--- src/main/java/my/photo_manager/PhotoManagerApplication.java
+++ src/main/java/my/photo_manager/PhotoManagerApplication.java
@@ -2,8 +2,10 @@ package my.photo_manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class PhotoManagerApplication {
 
 	public static void main(String[] args) {
