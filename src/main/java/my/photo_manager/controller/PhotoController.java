84a5be14c1f9b989324d59cf12a8a72c89d54758
8u3631984 <8u3631984@gmail.com>
2024-10-14 08:53:28 +0200

    add lombok annotation

--- src/main/java/my/photo_manager/controller/PhotoController.java
+++ src/main/java/my/photo_manager/controller/PhotoController.java
@@ -1,9 +1,13 @@
 package my.photo_manager.controller;
 
+import lombok.AccessLevel;
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import my.photo_manager.model.photo.Photo;
+import my.photo_manager.services.PhotoFilterService;
 import my.photo_manager.services.PhotoService;
+import my.photo_manager.web.PhotoDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,13 +19,11 @@ import java.util.stream.Collectors;
 import static org.apache.commons.io.FileUtils.readFileToByteArray;
 
 @Controller
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public class PhotoController {
 
     private final PhotoService photoService;
-
-    protected PhotoController(@NonNull PhotoService photoService) {
-        this.photoService = photoService;
-    }
+    private final PhotoFilterService metaDataFilterService;
 
     @RequestMapping(value = "/")
     public String index(Model model) {
